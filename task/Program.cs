34346ac74689ea287962b35b123e025b67a57c35
kisilelja <104495828+kisilelja@users.noreply.github.com>
2022-09-17 21:15:56 +0300

    Update Program.cs

--- task/Program.cs
+++ task/Program.cs
@@ -4,62 +4,55 @@
 // лучше обойтись исключительно массивами.
 
 Console.Clear();
-string[] ArrayStrings = new string[] { };
-Console.Write("Введите общее количество элементов массива: ");
-int n = int.Parse(Console.ReadLine());
-ArrayStrings = new string[n];
-for (int i = 0; i < ArrayStrings.Length; i++)
+Console.WriteLine("Сколько элементов будем в массиве?");
+int n = Convert.ToInt32(Console.ReadLine());
+string[] firstArray = new string[n];
+int i=0;
+while (i<firstArray.Length) 
+    {
+        Console.WriteLine($"введите {i}-й элемент массива:");
+        firstArray[i]=Console.ReadLine();
+        i++;
+    }
+Console.WriteLine("Ваш массив:");
+PrintArray (firstArray);
+void PrintArray(string[] array) 
 {
-    Console.Write($" Введите {i + 1} 'элемент: ");
-    ArrayStrings[i] = Console.ReadLine();
-}
-Console.WriteLine();
-Console.WriteLine("Массив введенных данных:");
-Console.WriteLine($"{PrintArray(ArrayStrings)}");
-Console.WriteLine();
-int length = 3;  
-int CheckArray(string[] array, int length)
-{
-    int result = 0;
+
     for (int i = 0; i < array.Length; i++)
     {
-        if (array[i].Length <= length) result++;
+        if (i != array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}.");
     }
-    return result;
+    Console.WriteLine();
 }
 
-int numbers = CheckArray(ArrayStrings, length); 
- 
-string[] newArrayStrings = new string[numbers]; 
-NewArray(ArrayStrings, newArrayStrings, length);
-
-
-void NewArray(string[] oldArray, string[] newArray, int lengthLimit)
+int CountStringLessThreeChar(string[] array) 
 {
-    int temp = 0;
-    for (int i = 0; i < oldArray.Length; i++)
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
     {
-        if (oldArray[i].Length <= lengthLimit)
+        if (array[i].Length <= 3) count++;
+    }
+    return count;
+}
+
+string[] FillResultArray(string[] array, int count)
+{
+    string[] result = new string[count];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
         {
-            newArray[temp] = oldArray[i];
-            temp++;
+            result[result.Length - count] = array[i];
+            count--;
         }
     }
-}
-
-string PrintArray(string[] array)
-{
-    string result = string.Empty;
-    result = "[ ";
-    for (int i = 0; i < array.Length; i++)
-    {
-        result += $"{array[i],1}";
-        if (i < array.Length - 1) result += ", ";
-    }
-    result += " ]";
     return result;
 }
 
-Console.WriteLine("Массив с элементами, длина которых меньше либо равна 3 символам:");
-Console.WriteLine($"{PrintArray(newArrayStrings)}");
+int countFirst = CountStringLessThreeChar(firstArray);
+string[] resultFirst =  FillResultArray(firstArray,countFirst);
+Console.Write ("Ваш новый массив: ");
+ PrintArray(resultFirst);
 
